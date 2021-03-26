68b57f29f0c7862b2dceda3c120019c9bbb83118
Mohammed Imran <mohammedimran86992@gmail.com>
2021-03-26 06:39:40 -0700

    Added color scheme for Hyper Terminal Emulator
    Signed-off-by: Mohammed Imran <mohammedimran86992@gmail.com>

---
+++ .hyper.js
@@ -0,0 +1,34 @@
+// Lighthaus Color theme for hyper
+// Author: Mohammed Imran (https://github.com/m0hammedimran)
+
+// Opacity or Alpha value only work for MacOS
+module.exports = {
+    config: {
+        cursorColor: '#FFFF00',
+        cursorShape: 'BLOCK',
+
+        foregroundColor: '#FFEE79',
+        backgroundColor: '#18191E',
+
+        selectionColor: '#FFEE79',
+
+        colors: {
+            black: '#18191E',
+            blue: '#1D918B',
+            cyan: '#00BFA3',
+            green: '#44B273',
+            magenta: '#D16BB7',
+            red: '#FC2929',
+            white: '#CCCCCC',
+            yellow: '#E25600',
+            lightBlack: '#21252D',
+            lightBlue: '#47A8A1',
+            lightCyan: '#5AD1AA',
+            lightGreen: '#50C16E',
+            lightMagenta: '#D68EB2',
+            lightRed: '#FF5050',
+            lightWhite: '#FFFADE',
+            lightYellow: '#ED722E',
+        },
+    },
+};
